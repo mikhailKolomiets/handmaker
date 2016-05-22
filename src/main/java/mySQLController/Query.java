3c3d24af89a323d7b534b452003962fb5ad2df40
Mihail Kolomiets <mihail.kolomiets@gmail.com>
2016-05-22 14:19:43 +0300

    VE-9 registration

--- src/main/java/mySQLController/Query.java
+++ src/main/java/mySQLController/Query.java
@@ -45,7 +45,7 @@ public class Query {
     public String createRegistration(String name, String password, String email, String town) throws Exception {
         connection = connectDB();
         statement = connection.createStatement();
-        String sql = "INSERT INTO handmaker.registration (name) VALUES (fff4)";
+        String sql = "INSERT INTO registration VALUES (NULL , NULL ,NULL ,NULL ,NULL , 'native')";
         statement.executeUpdate(sql);
         close();
         return null;
